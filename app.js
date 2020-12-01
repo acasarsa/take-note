a884c33b3db85096e571e1e93a5884d9c9e156e9
github username <github email address>
2020-11-30 23:13:15 -0500

    .

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 // fs.writeFileSync(nameOfFile, dataToWrite);
-fs.writeFileSync('notes.txt', 'My name is Andrew');
+// fs.writeFileSync('notes.txt', 'My name is Andrew');
+
+// challenge: append a message
+//
+// 1. use apendFileSync to append to notes.txt
+// const newLine = `\n`;
+const message = `this should be on the next line\n`;
+fs.appendFileSync('notes.txt', message);
